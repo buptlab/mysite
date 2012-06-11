f035437afc3b56350c39ee9d44ac6e958b277784
buptlab <scstscst24@gmail.com>
2012-06-11 22:22:33 +0800

    initial commit

---
+++ mysite/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+import datetime
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
+
+def hello(request):
+    return HttpResponse("Hello,world!")
